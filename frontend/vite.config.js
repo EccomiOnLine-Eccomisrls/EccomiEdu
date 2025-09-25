f72c25b04c135206849019f4c2fcc40596a144b5
Eccomi OnLine <ciaoeccomionline@gmail.com>
2025-09-26 01:49:27 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,11 @@
 export default {
-  server: { port: 5173, host: true }
+  server: { port: 5173, host: true },
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        admin: 'admin.html'
+      }
+    }
+  }
 };
