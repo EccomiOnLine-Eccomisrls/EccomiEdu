bdda70502e57bafdb0b4dff12a298f97cc9f476c
Eccomi OnLine <ciaoeccomionline@gmail.com>
2025-09-26 01:20:04 +0200

    Update main.js

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,4 @@
-const API_BASE = (window.VITE_API_BASE_URL) || "http://localhost:8000";
+const API_BASE = (import.meta.env.VITE_API_BASE_URL) || "http://localhost:8000";
 
 document.querySelector('#year').textContent = new Date().getFullYear();
 
